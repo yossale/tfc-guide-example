43be29bceb4b3936ab05da9c0961b140b2310ff7
Yossale <yossale@gmail.com>
2022-01-16 19:28:58 +0200

    update tp create lambda

--- variables.tf
+++ variables.tf
@@ -1,7 +1,10 @@
-variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
+variable "aws_region" {
+  description = "AWS region for all resources."
+
+  type    = string
+  default = "us-east-1"
 }
+
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
